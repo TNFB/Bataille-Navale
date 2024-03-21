d3eae053f2a602b457eaf016ace9011c1238368e
Chimorin <tom.herault4986@gmail.com>
2024-03-21 14:07:06 +0100

    Base de projet

---
+++ app/src/main/java/com/example/bataillenavale/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.bataillenavale;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.bataillenavale.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+    }
+}
